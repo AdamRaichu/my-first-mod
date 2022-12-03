0371408ed474e9960f1030da317dbe78fc005a2c
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-12-03 12:13:48 +0000

    add logger info

--- src/main/java/net/adamraichu/firstmod/FirstMod.java
+++ src/main/java/net/adamraichu/firstmod/FirstMod.java
@@ -5,11 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FirstMod implements ModInitializer {
-	public static final String MOD_ID = "firstMod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String MOD_ID = "firstmod";
+	public static final java.util.logging.Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		LOGGER.info("AdamRaichu mod initialized");
 	}
 }
