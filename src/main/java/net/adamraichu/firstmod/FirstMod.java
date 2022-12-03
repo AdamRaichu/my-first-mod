898f6623a0a47ec49aea26f1a1c887599bac39db
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-12-03 13:27:04 +0000

    lowercase {fabric.mod.json}.entrypoints.main[0]

--- src/main/java/net/adamraichu/firstmod/FirstMod.java
+++ src/main/java/net/adamraichu/firstmod/FirstMod.java
@@ -3,17 +3,17 @@ package net.adamraichu.firstmod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static net.minecraft.server.command.CommandManager.*;
+// import static net.minecraft.server.command.CommandManager.*;
 
 public class FirstMod implements ModInitializer {
   @Override
   public void onInitialize() {
-    CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("foo")
-        .executes(context -> {
-      // For versions below 1.19, replace "Text.literal" with "new LiteralText".
-      context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
+    // CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("foo")
+    //     .executes(context -> {
+    //   // For versions below 1.19, replace "Text.literal" with "new LiteralText".
+    //   context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
  
-      return 1;
-    })));
+    //   return 1;
+    // })));
   }
 }
