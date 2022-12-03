44cbfaa0d1861a2d94044c2329316e214bdda061
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-12-03 12:28:06 +0000

    change logger

--- src/main/java/net/adamraichu/firstmod/FirstMod.java
+++ src/main/java/net/adamraichu/firstmod/FirstMod.java
@@ -1,12 +1,14 @@
 package net.adamraichu.firstmod;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class FirstMod implements ModInitializer {
 	public static final String MOD_ID = "firstmod";
-	public static final java.util.logging.Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final java.util.logging.Logger LOGGER = Logger.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
