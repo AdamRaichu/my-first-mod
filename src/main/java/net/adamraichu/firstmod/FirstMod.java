0503e0230c7ecd795a4da0b55ec0b0b2bf8fc7eb
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-12-03 12:57:18 +0000

    comment out even more

--- src/main/java/net/adamraichu/firstmod/FirstMod.java
+++ src/main/java/net/adamraichu/firstmod/FirstMod.java
@@ -1,10 +1,10 @@
 package net.adamraichu.firstmod;
 
 import net.fabricmc.api.ModInitializer;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.*;
+// import java.io.IOException;
+// import java.util.logging.Level;
+// import java.util.logging.Logger;
+// import java.util.logging.*;
 
 public class FirstMod implements ModInitializer {
 	public static final String MOD_ID = "firstmod";
