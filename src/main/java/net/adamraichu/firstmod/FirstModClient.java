12c7d9637c724e0c78fb1e4c2de3e010315d7340
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-12-06 12:56:31 +0000

    [build] test of changes

--- src/main/java/net/adamraichu/firstmod/FirstModClient.java
+++ src/main/java/net/adamraichu/firstmod/FirstModClient.java
@@ -1,19 +1,20 @@
 package net.adamraichu.firstmod;
 
 import net.fabricmc.api.ClientModInitializer;
-import static net.minecraft.server.command.CommandManager.literal;
+// import static net.minecraft.server.command.CommandManager.*;
+// import com.mojang.brigadier.builder.ArgumentBuilder;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 public class FirstModClient implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
 
-    ClientCommandRegistrationCallback.EVENT.register ((dispatcher, registryAccess, environment) -> {
+    ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
       dispatcher.register(
-        literal("firstmod").executes(context -> {
-          context.getSource().sendMessage(Text.literal("Called /firstmod with no arguments"));
-        })
-      );
+          ClientCommandManager.literal("firstmod").executes(context -> {
+            context.getSource().sendMessage(ClientCommandManager.literal("Called /firstmod with no arguments"));
+          }));
     });
 
   }
